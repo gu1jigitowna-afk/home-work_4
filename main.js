70a456feb69107f6403ed239afb83c0aef1ea7c9
Твое имя <gu1jigitowna@gmail.com>
2026-03-23 17:09:57 +0600

    Добавлены файлы Index.html и main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let num = prompt("Сан киргизиңиз:")
+
+
+if (isNaN(num)){
+    alert("введите только числа")
+}else if (num % 2 === 0){
+    alert('Четное')
+} else {
+    alert('Нечетное')
+}
+ 
